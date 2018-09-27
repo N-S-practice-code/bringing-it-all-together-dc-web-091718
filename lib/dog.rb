66ec560708ff6cde5c01c821fae2880785d88c31
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-27 15:28:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -97,7 +97,7 @@ class Dog
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, id).map do |row|
+    DB[:conn].execute("SELECT * FROM dogs WHERE id = ? LIMIT 1", id).map do |row|
       self.new_from_db(row)
     end.first
   end
