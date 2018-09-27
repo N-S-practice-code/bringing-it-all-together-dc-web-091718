63040068b6c85bb9af0674993e4166acb8eff690
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-27 14:32:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = self.new(row[1], row[2])
+    new_dog = self.new(row[1], row[2])
     new_student.id = row[0]
     new_student 
   end
